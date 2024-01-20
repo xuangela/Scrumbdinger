ff47b24dc6f18c4979d59f6849ae4acfbb755ee5
Angela Xu <xuangela@gmail.com>
2024-01-20 12:26:12 -0800

    persisting data

--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -6,7 +6,9 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @Environment(\.scenePhase) private var scenePhase
     @State private var isPresentingNewScrumView = false
+    let saveAction: ()-> Void
   
     var body: some View {
         NavigationStack {
@@ -30,11 +32,15 @@ struct ScrumsView: View {
             NewScrumSheet(scrums: $scrums,
                           isPresentingNewScrumView: $isPresentingNewScrumView)
         }
+        .onChange(of: scenePhase) { phase in
+            if phase == .inactive {saveAction()}
+        }
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: .constant(DailyScrum.sampleData))
+        ScrumsView(scrums: .constant(DailyScrum.sampleData),
+                   saveAction: {})
     }
 }
